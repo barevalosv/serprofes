b0114ab51a479fd1c3ad91ce300d995e88ac6fe6
barevalosv <barevalosv11@gmail.com>
2026-05-12 21:30:03 +0200

    update 20260512

---
+++ 20260512/01-ejercicio/script.js
@@ -0,0 +1,27 @@
+//1. EL OBJETO
+//Usamos llaves {} para crear la ficha tecnica
+// y dos puntos para dar valor
+let producto = {
+    nombre: "🍎 Manzanas",
+    precio: 2.5,
+    categoria: "Fruta",
+};
+
+//2. ¿Cómo leemos un dato concreto y lo mandmos
+// al html?
+document.getElementById("prod-nombre").textContent=producto.nombre;
+document.getElementById("prod-precio").textContent=producto.precio;
+document.getElementById("prod-cat").textContent=producto.categoria;
+
+// MINI RETO //
+function mostrarMiFicha(){
+    let alumno = {
+        nombre: "Bryan",
+        edad: "23",
+        ciudad: "Madrid",
+    };
+
+    document.getElementById("alum-nombre").textContent=alumno.nombre;
+    document.getElementById("alum-edad").textContent=alumno.edad;
+    document.getElementById("alum-ciudad").textContent=alumno.ciudad;
+}
